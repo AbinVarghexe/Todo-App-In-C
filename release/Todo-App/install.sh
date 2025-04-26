9a57744e5e43c2b0775272278c52c567feaaaf35
mac <mac@Mac.Mac>
2025-04-26 03:29:52 -0400

    Revert "Prepare for v1.0 release"
    This reverts commit 1eba327b325ea702be7127d9df71db78e62e1533.

--- release/Todo-App/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Check for dependencies
-echo "Checking dependencies..."
-deps=("glfw" "opengl" "leif" "clipboard" "xcb")
-for dep in "${deps[@]}"; do
-  echo "Checking for $dep..."
-done
-echo "Run ./todo to start the application"
